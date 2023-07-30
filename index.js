f826d3de2903c1db9e506412067df8abfd060c34
Timothy Baker <97155523+TimBaker97@users.noreply.github.com>
2023-07-30 20:24:58 +0100

    Edited and updated

--- index.js
+++ index.js
@@ -1,34 +1,103 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
-let passOneEl = document.getElementById("passOne-el")
-let passTwoEl = document.getElementById("passTwo-el")
+let passwordOneField = document.getElementById("password-display-field-one");
+let passwordTwoField = document.getElementById("password-display-field-two");
 
+let passwordLength = 15;
+let includeNumbersInPassword = false;
+let includeLettersInPassword = false;
+let includeSpecialLettersInPassword = false;
 
-let passwordLength = 15
-
-function getRandomChar() {
-    let randomChar = Math.floor(Math.random() * characters.length)
-    return characters[randomChar]
-}
-
-function genFirstPass() {
-    let firstPass = ""
-    for (let i = 0; i < passwordLength; i++) {
-        firstPass += getRandomChar()
+function generatePassword() {
+    passwordOneField.textContent = "";
+    passwordTwoField.textContent = "";
+    if (characters.length === 0) {
+        passwordOneField.textContent = "No password generated";
+        passwordTwoField.textContent = "No password generated";
+    } else {
+        for (var i = 0; i < passwordLength; i++) {
+            passwordOneField.textContent += characters[Math.floor(Math.random() * characters.length)];
+            passwordTwoField.textContent += characters[Math.floor(Math.random() * characters.length)];
+        }
     }
-    return firstPass
 }
 
-function genSecondPass() {
-    let secondPass = ""
-    for (let i = 0; i < passwordLength; i++) {
-        secondPass += getRandomChar()
+function addMore() {
+    if (passwordLength < 18) {
+        document.getElementById("quantity").textContent = ++passwordLength;
+        document.getElementById("quantity").style.color = "#fff";
+    } else {
+        document.getElementById("quantity").style.color = "#9b2226";
     }
-    return secondPass
 }
 
-function renderPassword() {
-    passOneEl.textContent = genFirstPass()
-    passTwoEl.textContent = genSecondPass()
+function addLess() {
+    if (passwordLength > 5) {
+        document.getElementById("quantity").textContent = --passwordLength;
+        document.getElementById("quantity").style.color = "#fff";
+    } else {
+        document.getElementById("quantity").style.color = "#9b2226";
+    }
+}
+
+function numberOnly() {
+    passwordOneField.textContent = "";
+    passwordTwoField.textContent = "";
+    let OnOffNumbersSelector = document.getElementById("numbers");
+
+    if (includeNumbersInPassword) {
+        OnOffNumbersSelector.style.backgroundColor = "#9b2226";
+        OnOffNumbersSelector.textContent = "Numbers Removed";
+        includeNumbersInPassword = false;
+        characters.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    } else {
+        OnOffNumbersSelector.style.backgroundColor = "#10B981";
+        includeNumbersInPassword = true;
+        OnOffNumbersSelector.textContent = "Numbers Added";
+        characters = characters.filter(character => isNaN(Number(character)));
+    };
+};
+
+function lettersOnly() {
+    passwordOneField.textContent = "";
+    passwordTwoField.textContent = "";
+    let OnOffLettersSelector = document.getElementById("letters");
+
+    if (includeLettersInPassword) {
+        OnOffLettersSelector.style.backgroundColor = "#9b2226";
+        OnOffLettersSelector.textContent = "Letters Removed";
+        includeLettersInPassword = false;
+        characters = characters.concat(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]);
+    } else {
+        OnOffLettersSelector.style.backgroundColor = "#10B981";
+        includeLettersInPassword = true;
+        OnOffLettersSelector.textContent = "Letters Added";
+        characters = characters.filter(character => !(/[a-zA-Z]/.test(character)));
+    };
+};
+
+
+function specialOnly() {
+    passwordOneField.textContent = "";
+    passwordTwoField.textContent = "";
+    let OnOffSpecialCharachtersSelector = document.getElementById("special");
+    const special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+    if (includeSpecialLettersInPassword) {
+
+        OnOffSpecialCharachtersSelector.style.backgroundColor = "#9b2226";
+        OnOffSpecialCharachtersSelector.textContent = "Special Characters Removed";
+        includeSpecialLettersInPassword = false;
+        characters.push(...special);
+    } else{
+        OnOffSpecialCharachtersSelector.style.backgroundColor = "#10B981";
+        includeSpecialLettersInPassword = true;
+        OnOffSpecialCharachtersSelector.textContent = "Special Characters Added";
+        characters = characters.filter(character => !special.includes(character));
+    }
+}
+
+function clearPassword() {
+    passwordOneField.textContent = "";
+    passwordTwoField.textContent = "";
 }
